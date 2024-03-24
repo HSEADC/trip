72f729ca7e038365e74445c68fc4c62b069e3ebe
Arina <arina04arina@gmail.com>
2024-03-25 02:02:50 +0300

    проверка адаптива на телефоне

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import './index.css'
+var jQueryScript = document.createElement('script');
+jQueryScript.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+jQueryScript.type = 'text/javascript';
+
+var jQueryUIScript = document.createElement('script');
+jQueryUIScript.src = 'https://code.jquery.com/ui/1.12.1/jquery-ui.js';
+jQueryUIScript.type = 'text/javascript';
+
+document.head.appendChild(jQueryScript);
+document.head.appendChild(jQueryUIScript);
+
+$(document).ready(function() {
+  $(".Q_Icons-Burger").click(function() {
+    $(".menu-mobile").hide();
+    $(".blur-mobile").show();
+    $(".none").show();
+  });
+
+  $(".close").click(function() {
+    $(".menu-mobile").show(); 
+    $(".blur-mobile").hide(); 
+    $(".none").hide();
+  });
+  
+});
 
 
 
